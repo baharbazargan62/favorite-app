9c01d88054cbe8447c31ff24273d0fa66a6c80fa
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-06-21 13:15:10 +0100

    set routes

--- favorite-app/src/App.js
+++ favorite-app/src/App.js
@@ -1,8 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
 
 function App() {
   return (
-    <div>Hello!</div>
+    <div>
+      <Routes>
+        <Route path="/" exact={true} />
+        <Route path="/allmeetings" component={AllMeetups} />
+        <Route path="/favorites" component={Favorites} />
+        <Route path="/new-meetings" component={NewMeetup} />
+      </Routes>
+    </div>
   );
 }
 

b6e7ce4648749c6abac19afea0c1f896bccf8e52
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-06-23 14:59:46 +0100

    add set state

--- favorite-app/src/pages/AllMeetups.js
+++ favorite-app/src/pages/AllMeetups.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MeetUpList from "../components/layouts/MeetUpList"
+import { useState } from 'react';
 const DUMMY_DATA = [
   {
     id: 'm1',
@@ -21,10 +22,27 @@ const DUMMY_DATA = [
   },
 ];
 function AllMeetups() {
-  return (
+const [loading,setLoading]=useState(true)
+const [meetups,setMeetup]=useState([])
+  fetch(
+    "https://favorite-app-a4658-default-rtdb.firebaseio.com/meetups.json"
+  ).then((response)=>{
+     return response.json
+    }).then((data)=>{
+      setLoading(false)
+      setMeetup(data)
+    }); 
+  if (loading){
+        return(
+          <section>
+            <p>loading ....</p>
+          </section>
+        )
+      }
+      return (
     <div>
    <h1>All meeting</h1>
-   <MeetUpList meetups={DUMMY_DATA}/>
+   <MeetUpList meetups={meetups}/>
 
     </div>
   )

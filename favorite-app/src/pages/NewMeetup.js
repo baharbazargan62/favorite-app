8024cdb046a8f288708a0a23da1f9758973c59d1
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-06-23 13:39:44 +0100

    Add fetch data

--- favorite-app/src/pages/NewMeetup.js
+++ favorite-app/src/pages/NewMeetup.js
@@ -2,15 +2,26 @@ import React from 'react'
 import NewMeetingForm from '../components/layouts/NewMeetingForm'
 
 function NewMeetup() {
-  function addMeetupHandler(){
+  function getResponse(meetupData) {
+      fetch(
+        "https://favorite-app-a4658-default-rtdb.firebaseio.com/meetups.json",
+        {
+          method: 'POST',
+          body: JSON.stringify(meetupData),
+          headers: {
+            'Content-Type': 'application/json'
+            
+          }
+        }
+      );  
+    }
 
-  }
   return (
     <div> 
       <h3>Add New Meeting</h3>
-      <NewMeetingForm onAddMeetup={addMeetupHandler}/>
+      <NewMeetingForm onAddMeetup={getResponse}/>
     </div>
   )
-}
+  }
 
 export default NewMeetup

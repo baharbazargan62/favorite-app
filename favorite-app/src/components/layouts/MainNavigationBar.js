e8b4a6b6fe96f053d82f44cb802ea1ad338e1021
baharbazargan62 <105979952+baharbazargan62@users.noreply.github.com>
2023-06-28 15:15:14 +0100

    add badge

--- favorite-app/src/components/layouts/MainNavigationBar.js
+++ favorite-app/src/components/layouts/MainNavigationBar.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./MainNavigationBar.module.css";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-contxt";
 
 function MainNavigationBar() {
+  const favoriteCtx=useContext(FavoritesContext);
   return (
     <header className={classes.header}>
         <div className={classes.logo}>BAHAR  BAZARGAN </div>
@@ -15,7 +18,7 @@ function MainNavigationBar() {
     <Link to="/new-meetings">Add New Meetup </Link>
    </li>
    <li>
-    <Link to="/favorites">Favorites</Link>
+    <Link to="/favorites">My Favorites<span className={classes.badge}>{favoriteCtx.totalFavorites}</span></Link>
    </li>
  </ul> 
 </nav>
